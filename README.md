08524f2b8201860361bca72e50f8fd40ee7cb929
Emily Burns <Emily.Burns@veritas.com>
2016-06-24 16:03:34 -0700

    microservice with docker, redis, flask

---
+++ README.md
@@ -0,0 +1,9 @@
+# This is a sample project
+This project is (or will be) a small containerized microservice running in docker.
+It is meant to test the surf pipelines.
+It will serve as a base project to kickstart development of similar projects.
+It will be awesome.
+
+
+### Naming
+"Kook" is a slang term for a novice surfer - http://www.urbandictionary.com/define.php?term=kook
---
+++ run.sh
@@ -0,0 +1,25 @@
+# Copyright 2016 Veritas Technologies LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/bash
+
+docker stop kook-frontend
+docker stop kook-db
+
+docker rm kook-frontend
+docker rm kook-db
+
+# Run
+docker run -d --name kook-db redis
+docker run -d -p 5000:5000 --name kook-frontend --link kook-db:kook-db kook-frontend
